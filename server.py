1b912eec1c33366a8e884322bc50775ed3c96ab6
yoshiyuki-nakamoto <nakamoto6290@gmail.com>
2021-05-22 23:28:05 +0900

    server

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, jsonify
+from proofreading import Proofreading
+
+app = Flask(__name__)
+pr = Proofreading()
+
+
+@app.route('/proofreading_run', methods=["POST"])
+def proofreading_run():
+    input_text = request.json["text"]
+
+    result_text = pr.proof_with_word_base(input_text)
+
+    return jsonify({"result": result_text})
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', title='proofreading_tool')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
