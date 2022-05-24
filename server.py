26329c157e34563a87d9e485e93584d297c07332
yoshiyuki-nakamoto <66869101+yoshiyuki-nakamoto@users.noreply.github.com>
2022-05-25 02:00:25 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-from proofreading import proofreading
+from proofreading import Proofreading
 
 app = Flask(__name__)
 pr = Proofreading()
